76f1d0d35442e11c936d9425f15c34fdb4dc776a
pandistic-zain <zainsaith7@gmail.com>
2024-05-14 06:37:54 -0700

    commit

---
+++ Collections_Practice/List.java
@@ -0,0 +1,15 @@
+package Collections_Practice;
+
+import java.util.ArrayList;
+
+public class List {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> l = new ArrayList<>();
+        l.add(22);
+        l.add(45);
+        l.add(1 , 34);
+        l.add(67);
+        System.out.println("List is : "+l);
+    }
+}
